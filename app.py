ad2923aa96beef6b38bcc8f5ee9e21fd252da281
Oluwaseun Musa <80710703+superissy@users.noreply.github.com>
2023-12-13 22:35:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import boto3 
 import json 
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 
-load_dotenv()
+# load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
+# API_KEY = os.getenv('API_KEY')
 
-print(API_KEY)
+# print(API_KEY)
 
+print("Hello World")
