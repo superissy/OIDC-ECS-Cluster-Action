efcab6881e0c28897eb348793430ee22ee64d22c
Oluwaseun Musa <imusa1@wgu.edu>
2024-01-23 22:50:28 -0600

    Removed Duplicated Terraform outputs

--- output.tf
+++ output.tf
@@ -6,18 +6,6 @@ output "id" {
   value = aws_ecs_cluster.owasp-juice-shop.id
 }
 
-output "name" {
-  value = aws_ecs_service.owasp-juice-shop-service.name
-}
-
-output "id" {
-  value = aws_ecs_service.owasp-juice-shop-service.id
-}
-
-output "name" {
-  value = aws_ecs_task_definition.owasp_juice_shop_task.name
-}
-
-output "id" {
-  value = aws_ecs_task_definition.owasp_juice_shop_task.id
+output "arn" {
+  value = aws_ecs_cluster.owasp-juice-shop.arn
 }
